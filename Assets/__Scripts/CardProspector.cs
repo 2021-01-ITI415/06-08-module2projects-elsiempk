7302a39aaa800102dc5c2a49e95ee8d29a6c16e2
elsiempk <elsiepk@gmail.com>
2021-03-08 22:30:22 -0500

    a subclass of card
    - new CardProspector script
    - edit prospector script to convert the cards in the deck from cards to cardprospectors
    - add cardprospector to prefabcard prefab inspector and remove the old card script then when you press play and check drawpile within prospector script in main camera nothing is null and it is full of cardprospectors

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// an enum defines a variable type with a few prenamed values
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card
+{
+    // make sure cardprospector extends card
+    [Header("Set Dynamically: CardProspector")]
+    // this is how you use the enum ecardstate
+    public eCardState state = eCardState.drawpile;
+    // the hiddenby list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // the layoutid matches this card to the tableau xml if it's a tableau card
+    public int layoutID;
+    // the slotdef class stores information pulled in from the layoutxml <slot>
+    public SlotDef slotDef;
+}
+
